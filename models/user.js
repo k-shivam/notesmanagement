7deb029b5103a95b367acaab636aa12b35abf2d6
k-shivam <42085548+k-shivam@users.noreply.github.com>
2024-01-04 18:20:37 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        unique: true,
+        required: true
+    },
+    password:{
+        type:String,
+        required: true
+    }
+});
+
+
+userSchema.methods.comparePassword = (password) =>{
+    return bcrypt.compare(password, this.password)
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
